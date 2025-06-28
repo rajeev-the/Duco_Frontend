f55c18cd2668bdd0771cdc909f8d8d9ef5eec878
Rajeev <rajeevranjan9560807144@gmail.com>
2025-06-28 13:34:23 +0530

    first commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import SectionHome1 from '../Components/SectionHome1.jsx'
 
 const Home = () => {
   return (
-    <div className='h-full bg-[black] w-full  '>
+    <div className='h-full bg-[#0A0A0A] w-full  '>
         <SectionHome1/>
 
         <div  className='h-20'>
