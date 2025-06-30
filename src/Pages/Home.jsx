404136f6219fd3ee7faccc7787be7cc75708eb1e
Rajeev <rajeevranjan9560807144@gmail.com>
2025-06-30 20:16:39 +0530

    first commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import SectionHome1 from '../Components/SectionHome1.jsx'
 import SectionHome2 from '../Components/SectionHome2.jsx'
+import SectionHome3 from '../Components/SectionHome3.jsx'
 
 const Home = () => {
   return (
     <div className='h-full bg-[#0A0A0A] w-full  '>
         <SectionHome1/>
         < SectionHome2/>
+        <SectionHome3/>
+
+        <div>
+          jijijbk
+        </div>
         
       
 
