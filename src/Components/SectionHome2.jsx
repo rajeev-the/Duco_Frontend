28dccfeb4cd135d5c07f997201920927c21cac5a
Rajeev <rajeevranjan9560807144@gmail.com>
2025-07-01 01:09:21 +0530

    first commit

--- src/Components/SectionHome2.jsx
+++ src/Components/SectionHome2.jsx
@@ -1,23 +1,33 @@
 import React from 'react'
 import BoxOfProdcuts from './BoxOfProdcuts'
+import { ChevronRight } from 'lucide-react';
 
 const SectionHome2 = () => {
   return (
- <section className="mt-10 px-4 md:px-8 lg:px-16">
+<section className="mt-10 px-4 md:px-8 lg:px-16">
   <h1 className="text-3xl text-white text-center font-semibold">Shop Now</h1>
 
+  {/* Product Grid */}
   <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6 mt-8">
     <BoxOfProdcuts />
     <BoxOfProdcuts />
     <BoxOfProdcuts />
     <BoxOfProdcuts />
     <BoxOfProdcuts />
-        <BoxOfProdcuts />
     <BoxOfProdcuts />
     <BoxOfProdcuts />
- 
+    <BoxOfProdcuts />
+  </div>
+
+  {/* Button Wrapper */}
+  <div className="mt-6 flex justify-end">
+    <button className="flex items-center gap-2 px-4 py-2 bg-white text-black rounded-xl shadow-sm border font-semibold hover:bg-gray-100 transition">
+      Explore All
+      <ChevronRight size={20} />
+    </button>
   </div>
 </section>
+
 
   )
 }
--- src/Components/SectionHome3.jsx
+++ src/Components/SectionHome3.jsx
@@ -7,7 +7,7 @@ import sale from "../assets/happy-beautiful-couple-posing-with-shopping-bags-vio
 
 const SectionHome3 = () => {
   return (
-     <div className='mt-[100px] px-4 sm:px-4 md:px-6 lg:px-16 w-full flex justify-center items-center'>
+     <div className='mt-[100px] px-4 md:px-8 lg:px-16 w-full flex justify-center items-center'>
 
   <div className='flex flex-col md:flex-row w-full max-w-screen-xl gap-3'>
 
@@ -18,13 +18,13 @@ const SectionHome3 = () => {
       <div className='bg-white rounded-xl  flex flex-col sm:flex-row items-center justify-between h-full shadow-sm relative'>
 
         {/* Text */}
-        <div className="flex flex-col px-6 justify-center  text-left  ">
+        <div className="flex flex-col px-6 justify-center  sm:mr-0 mr-[135px]  text-left  ">
           <p className="text-5xl sm:text-5xl font-bold">SALE</p>
           <p className="text-5xl sm:text-5xl font-semibold">20% OFF</p>
         </div>
 
         {/* Image */}
-        <img src={sale} alt="Sale" className='h-[150px] sm:h-[250px] md:h-[200px] object-contain' />
+        <img src={sale} alt="Sale" className='h-[155px] sm:h-[200px] md:h-[200px] object-contain' />
 
         {/* Badge */}
         <div className="absolute top-2 right-2">
@@ -39,14 +39,14 @@ const SectionHome3 = () => {
       <div className='bg-white rounded-xl  flex flex-col sm:flex-row items-center justify-between h-full shadow-sm relative'>
 
         {/* Text */}
-        <div className="flex flex-col text-left px-4   sm:text-left">
+        <div className="flex flex-col text-left px-4   sm:mr-0 mr-[160px] sm:text-left">
           <p className="text-5xl sm:text-6xl font-bold leading-tight">Get</p>
           <p className="text-5xl sm:text-6xl font-bold leading-tight">BULK</p>
           <p className="text-5xl sm:text-6xl font-bold leading-tight">T-SHIRT</p>
         </div>
 
         {/* Image */}
-        <img src={bulk} alt="Bulk T-shirt" className='h-[200px] sm:h-[240px] object-contain mt-[50px] sm:mt-0  ' />
+        <img src={bulk} alt="Bulk T-shirt" className='h-[200px] sm:h-[300px] object-contain mt-[50px] sm:mt-0  ' />
 
         {/* Badge */}
         <div className="absolute top-2 right-2">
@@ -63,7 +63,7 @@ const SectionHome3 = () => {
     <div className='w-full md:w-[25%] bg-white rounded-xl  shadow-sm flex flex-col justify-between relative'>
 
       {/* Text */}
-      <div className='w-full p-4'>
+      <div className='w-full  p-4'>
         <div className='flex justify-between items-center'>
           <p className="text-5xl sm:text-5xl font-semibold">Get</p>
           <div className="bg-[#E5C870] w-10 h-10 rounded-full flex items-center justify-center">
