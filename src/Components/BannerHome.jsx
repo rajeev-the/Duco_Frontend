2d971d3bd807280521d35933698005474f05f550
rajeev-the <146732212+rajeev-the@users.noreply.github.com>
2026-05-25 01:37:50 +0530

    banner_update

--- src/Components/BannerHome.jsx
+++ src/Components/BannerHome.jsx
@@ -1,17 +1,18 @@
+
 import React from 'react'
 import banner from '../WhatsApp Image 2026-05-25 at 1.24.09 AM.jpeg'
-const BannerHome = ({link}) => {
+
+const BannerHome = () => {
   return (
     <section
-  className="relative mt-[100px] sm:mt-10 px-4 md:px-8 lg:px-16 bg-center bg-cover bg-no-repeat aspect-[16/9]"
-  style={{
-    backgroundImage: `url(${link})`
-  }}
->
-  {/* Dark overlay (optional, remove if not needed) */}
-  <div className="absolute inset-0 bg-black/30"></div>
-</section>
-
+      className="relative mt-[100px] sm:mt-10 px-4 md:px-8 lg:px-16 bg-center bg-cover bg-no-repeat aspect-[16/9]"
+      style={{
+        backgroundImage: `url(${banner})`
+      }}
+    >
+      {/* Dark overlay */}
+      <div className="absolute inset-0 bg-black/30"></div>
+    </section>
   )
 }
 
